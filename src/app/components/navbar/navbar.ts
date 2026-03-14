a3b175e11679cc496637c6be31cafab1405f65d3
JarethJaziel <jarethmoo@gmail.com>
2026-03-14 13:03:13 -0600

    fix navbar mobile (contact) and adjust contact available feature

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -9,5 +9,23 @@ import { SocialSidebar } from '../social-sidebar/social-sidebar';
   styleUrl: './navbar.css',
 })
 export class Navbar {
+
   @Input() sidebarRef!: SocialSidebar;
+
+  onContactClick($event: PointerEvent) {
+    if (window.innerWidth < 768) {
+      // móvil → ir a sección contact
+      globalThis.location.hash = 'contact';
+    } else {
+      // desktop → hacer shake
+      $event.preventDefault();
+      this.sidebarRef.triggerShake();
+    }
+  }
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
